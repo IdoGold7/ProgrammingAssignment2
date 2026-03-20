8121417e85d2b4206ca5278e33deca76fe37edfe
Ido Goldberg <ido.goldberg.90@gmail.com>
2026-03-20 13:00:00 +0200

    add comments and fix typo line 50

--- cachematrix.R
+++ cachematrix.R
@@ -4,20 +4,32 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  #Creates a list of 4 functions: set and get a metrix; 
+  #set and get an inverse of the matrix.
+  #returns the list
+  
   inv_matrix <- NULL
   setmatrix <- function(y) {
+    #Update x and inv_matrix in parent env.
+    #if func gets a new matrix, it invalidates the cached inverse matrix
+    
     x <<- y
     inv_matrix <<- NULL
   }
   getmatrix <- function() x
+  #Returns the matrix
+  
   setinverse <- function(solve) inv_matrix <<- solve
+  #calculates the inverse and update outside scope
+  
   getinverse <- function() inv_matrix
+  #returns the inversed matrix
+  
   list(setmatrix = setmatrix, 
        getmatrix = getmatrix,
        setinverse = setinverse,
        getinverse = getinverse)
 }
-
 
 ## Write a short comment describing this function
 
@@ -25,13 +37,17 @@ cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
   
   inv_matrix <- x$getinverse()
+  
+  #checking for cached matrix inverse, if inverse isn't NULL, returns it
   if(!is.null(inv_matrix)) {
     message("getting cached data")
     return(inv_matrix)
   }
+  
+  #If inv_matrix is NULL, calculates the inverse and caches result 
   data <- x$getmatrix()
   inv_matrix <- solve(data, ...)
-  x$setinvers(inv_matrix)
+  x$setinverse(inv_matrix)
   inv_matrix
 }
 
